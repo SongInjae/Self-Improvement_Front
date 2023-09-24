64576e3b1fa9c26bf2f76bea355b6b054083727c
SongInJae <pan0663@naver.com>
2023-09-24 22:26:16 +0900

    style: recet.css적용 및 font 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter } from 'react-router-dom';
-import {router } from './routes';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <createBrowserRouter router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

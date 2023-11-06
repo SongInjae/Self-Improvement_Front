80df56a9e620b0aa8e3c9187f73cb7fe21712722
SongInJae <pan0663@naver.com>
2023-11-06 22:37:34 +0900

    feat: main ui 완성

--- src/pages/TodayPlanPage/index.tsx
+++ src/pages/TodayPlanPage/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { ORIGINAL_YELLOW, PASTEL_ORANGE } from '../../constants';
+import RepeatModal from '../../components/RepeatModal';
+import { useState } from 'react';
 
 const TodayPlanContainer = styled.form`
   display: flex;
@@ -27,6 +29,7 @@ const HalfInputWrapper = styled.div`
 `;
 const HalfInput = styled(InputStyled)`
   width: 50%;
+  height: 3.5rem;
 
   &:first-child {
     border: none;
@@ -35,6 +38,12 @@ const HalfInput = styled(InputStyled)`
     border-top-left-radius: 0;
     border-top-right-radius: 0;
   }
+`;
+const RepeatInput = styled.div`
+  width: 50%;
+  height: 3.5rem;
+  border-bottom: 1px solid ${ORIGINAL_YELLOW};
+  box-sizing: border-box;
 `;
 const FieldSet = styled.div`
   border-bottom: 1px solid ${ORIGINAL_YELLOW};
@@ -72,6 +81,7 @@ const SubmitButton = styled.button`
 `;
 
 const TodayPlanPage = () => {
+  const [showModal, setShowModal] = useState(false);
   const interest_list = [
     {
       interest: '주식',
@@ -103,31 +113,34 @@ const TodayPlanPage = () => {
     },
   ];
   return (
-    <TodayPlanContainer>
-      <InputStyled type="text" placeholder="계획 제목 추가" />
-      <InputStyled type="text" placeholder="세부 계획 추가" />
-      <HalfInputWrapper>
-        <HalfInput type="time" placeholder="시간 설정" />
-        <HalfInput type="number" placeholder="반복" />
-      </HalfInputWrapper>
-      <FieldSet>
-        <Title>관심사 선택</Title>
-        <RadioWrapper>
-          {interest_list.map((interest) => (
-            <Radio key={interest.interest} check={interest.checked}>
-              {interest.interest}
-            </Radio>
-          ))}
-        </RadioWrapper>
-      </FieldSet>
-      <FieldSet>
-        <Title>태그 추가</Title>
-        <RadioWrapper>
-          <Radio>+</Radio>
-        </RadioWrapper>
-      </FieldSet>
-      <SubmitButton type="submit">완료</SubmitButton>
-    </TodayPlanContainer>
+    <>
+      <TodayPlanContainer>
+        <InputStyled type="text" placeholder="계획 제목 추가" />
+        <InputStyled type="text" placeholder="세부 계획 추가" />
+        <HalfInputWrapper>
+          <HalfInput type="time" placeholder="시간 설정" />
+          <RepeatInput onClick={() => setShowModal(true)} />
+        </HalfInputWrapper>
+        <FieldSet>
+          <Title>관심사 선택</Title>
+          <RadioWrapper>
+            {interest_list.map((interest) => (
+              <Radio key={interest.interest} check={interest.checked}>
+                {interest.interest}
+              </Radio>
+            ))}
+          </RadioWrapper>
+        </FieldSet>
+        <FieldSet>
+          <Title>태그 추가</Title>
+          <RadioWrapper>
+            <Radio>+</Radio>
+          </RadioWrapper>
+        </FieldSet>
+        <SubmitButton type="submit">완료</SubmitButton>
+      </TodayPlanContainer>
+      <RepeatModal showModal={showModal} setShowModal={setShowModal} />
+    </>
   );
 };
 

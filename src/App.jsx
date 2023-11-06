e2078cb1be67dc6bc8e6ac6f6af8a83e0e14403c
SongInJae <pan0663@naver.com>
2023-11-07 00:59:48 +0900

    fix: 파일 확장자 jsx로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from '@emotion/styled';
 import BottomNavigation from './components/BottomNavigation';
-import Header from './components/Header';
 
 const Background = styled.div`
   max-width: 425px;

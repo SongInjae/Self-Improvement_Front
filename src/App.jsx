acc158dee837e1767ad9c9a724ff754be0f365dc
SongInJae <pan0663@naver.com>
2023-11-07 00:34:50 +0900

    feat: 공통 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from '@emotion/styled';
+import BottomNavigation from './components/BottomNavigation';
+import Header from './components/Header';
 
 const Background = styled.div`
   max-width: 425px;
   height: 100vh;
   border: 1px solid black;
   margin: 0 auto;
-`
+`;
 
 const App = () => {
   return (
-    <Background><Outlet /></Background>
+    <Background>
+      <Outlet />
+      <BottomNavigation />
+    </Background>
   );
 };
 

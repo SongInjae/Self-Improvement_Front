a9f91782f655ce32314d37961b0057bf7e6c6d6b
SongInJae <pan0663@naver.com>
2023-12-08 14:37:42 +0900

    2차 main merge (#22)
    * feat: 로그인 및 회원가입 API 모킹 및 목표 추가 API 일부 구현
    
    * feat: 오늘 계획 추가 기능 구현
    
    * feat: calendar props context로 변경
    
    * feat: 계획 추가 및 불러오기 연결
    
    * 달력 선택시 해당 날짜의 todo 불러오는 API 구현
    
    * remove .env file from git repo
    
    * fix: today 계획 페이지 디자인 수정
    
    * feat: 입력 창 구현
    
    * feat: 태그 페이지 UI 구현
    
    * 사진 페이지 UI 완성
    
    * fix: height 오류 수정
    
    * feat: 목표페이지 ui 구현 (#14)
    
    * fix: 일단 중간 점검 (#15)
    
    * Conflict1 (#18)
    
    * feat: 목표페이지 ui 구현
    
    * feat: HAGYOUNG
    
    * feat: HAGYOUNG
    
    * fix: 삭제할 파일들 삭제
    
    ---------
    
    Co-authored-by: hagyeong kim <hagyeong032p@naver.com>
    
    * Jxxhyo123 중간점검 완료 (#19)
    
    * fix: 재효 중간점검
    
    * fix: 충돌 해결
    
    * Post detail page (#20)
    
    * fix: UserInfo 컴포넌트 분리
    
    * feat: post 상세 페이지 UI 완료
    
    * feat: 댓글 페이지 UI 구현 완료
    
    * fix: TodoList 컴포넌트 분리
    
    * fix: vercel route 오류 해결
    
    ---------
    
    Co-authored-by: hg0320 <142627892+hg0320@users.noreply.github.com>
    Co-authored-by: hagyeong kim <hagyeong032p@naver.com>
    Co-authored-by: jxxhyo <122712519+jxxhyo@users.noreply.github.com>

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,110 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import getTodayPlan from '../../apis/schedule/getTodayPlan';
+import CalendarContext from '../../context/CalendarContext';
+import { transformDate } from '../../utils/transform';
+import styled from '@emotion/styled';
+import { ORIGINAL_YELLOW, PASTEL_ORANGE } from '../../constants/color';
+
+const TodoWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 95%;
+  margin: 0 auto;
+  padding: 1rem 1.5rem;
+  border-radius: 2rem;
+  box-sizing: border-box;
+  background-color: ${({ check }) => (check ? ORIGINAL_YELLOW : PASTEL_ORANGE)};
+`;
+const TodoItem = styled.div`
+  color: white;
+  font-family: 'MainBold';
+`;
+const TagWrapper = styled.div`
+  display: flex;
+  margin-top: 0.3rem;
+`;
+const Tag = styled.div`
+  font-size: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: lightgray;
+  padding: 0.3rem;
+`;
+const Checkbox = styled.div`
+  position: absolute;
+  right: 2rem;
+  bottom: 50%;
+  transform: translateY(50%);
+  width: 1.5rem;
+  height: 1.5rem;
+  color: ${ORIGINAL_YELLOW};
+  border-radius: 0.25rem;
+  background-color: white;
+  cursor: pointer;
+
+  &::after {
+    content: '✔︎';
+    display: ${({ check }) => (check ? 'block' : 'none')};
+    text-align: center;
+    line-height: 1.5rem;
+  }
+`;
+
+const AddTodo = styled.div`
+  width: 95%;
+  height: 3rem;
+  margin: 0 auto;
+  line-height: 3rem;
+  border-radius: 2rem;
+  color: ${ORIGINAL_YELLOW};
+  background-color: rgba(232, 232, 232, 0.6);
+  text-align: center;
+  font-size: 2rem;
+  cursor: pointer;
+`;
+
+const TodoList = () => {
+  const [todoList, setTodoList] = useState([]);
+  const navigate = useNavigate();
+  const { state } = useContext(CalendarContext);
+  const { selectDay } = state;
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getTodayPlan({ date: transformDate(selectDay) });
+      setTodoList(data);
+    };
+
+    getData();
+  }, [selectDay]);
+
+  const handleCheckChange = (idx) => {
+    setTodoList((prevState) => {
+      const newState = [...prevState];
+      newState[idx] = {
+        ...newState[idx],
+        checked: !prevState[idx].checked,
+      };
+
+      return newState;
+    });
+  };
+  return (
+    <>
+      {todoList.map((item, idx) => (
+        <TodoWrapper key={item.title} check={item.checked}>
+          <TodoItem>{item.title}</TodoItem>
+          <Checkbox
+            check={item.checked}
+            onClick={() => handleCheckChange(idx)}
+          />
+        </TodoWrapper>
+      ))}
+      <AddTodo onClick={() => navigate('/todayplan')}>+</AddTodo>
+    </>
+  );
+};
+
+export default TodoList;

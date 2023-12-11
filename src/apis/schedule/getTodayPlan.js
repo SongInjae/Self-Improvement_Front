e06946dd9f2e4b21bd2a77aa422b1aa8af6097b6
SongInJae <pan0663@naver.com>
2023-12-12 00:44:03 +0900

    fix: 스케줄 확인 API 변동

--- src/apis/schedule/getTodayPlan.js
+++ src/apis/schedule/getTodayPlan.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { axiosClient } from '../axiosClient';
 
 const getTodayPlan = async ({ date, userId }) => {
+<<<<<<< HEAD
   const { data } = await axiosClient.get('/api/schedule/scheduleByDate', {
+=======
+  const { data } = await axiosClient.get('api/schedule/scheduleByDate', {
+>>>>>>> b0d6c92 (fix: 스케줄 확인 API 변동)
     params: { Date: date, userId },
   });
 
